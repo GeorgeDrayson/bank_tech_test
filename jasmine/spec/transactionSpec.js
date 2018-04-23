856b8617fe76cd6f42c0ffa1557f4f97823e4ba7
George Drayson <drayson.g@mac.com>
2018-04-23 15:01:00 +0100

    Put deposit and withdrawal classes into one class called transaction

--- jasmine/spec/transactionSpec.js
+++ jasmine/spec/transactionSpec.js
@@ -1,5 +1,5 @@
-describe("Deposit", function() {
-  var deposit;
+describe("Transaction", function() {
+  var transaction;
 
   beforeEach(function() {
     date = jasmine.createSpyObj('date',['getDate','getMonth','getFullYear']);
@@ -8,13 +8,18 @@ describe("Deposit", function() {
     date.getFullYear.and.returnValue(1999);
     formatter = jasmine.createSpyObj('formatter',['format']);
     formatter.format.and.returnValue('01');
-    deposit = new Deposit(1000, 1000, date, formatter);
+    deposit = new Transaction(1000, 1000, date, formatter);
+    withdrawal = new Transaction(-1000, 0, date, formatter);
   });
 
   describe("#getAmount", function() {
 
     it("returns the amount", function() {
       expect(deposit.getAmount()).toEqual(1000);
+    });
+
+    it("can take negative values", function() {
+      expect(withdrawal.getAmount()).toEqual(-1000);
     });
 
   });
--- jasmine/src/transaction.js
+++ jasmine/src/transaction.js
@@ -1,4 +1,4 @@
-function Deposit(
+function Transaction(
   amount, balance, date=new Date(), formatter=new DateFormatter()) {
   this._amount = amount
   this._balance = balance
@@ -6,15 +6,15 @@ function Deposit(
   this._formatter = formatter
 }
 
-Deposit.prototype.getAmount = function() {
+Transaction.prototype.getAmount = function() {
   return this._amount
 }
 
-Deposit.prototype.getBalance = function() {
+Transaction.prototype.getBalance = function() {
   return this._balance
 }
 
-Deposit.prototype.date = function() {
+Transaction.prototype.date = function() {
   return formatter.format(date.getDate()) + "/" + formatter.format(date.getMonth())
    + "/" + date.getFullYear()
 }
